713488b7078f46dffd103ad07a1ab85cd09f4ef5
abdulaziz-ux <abdulaljizi@gmail.com>
2024-11-10 22:23:13 +0700

    Add files via upload

---
+++ visual Desktop/AbdulAziz_UTS Visual Desktop.Designer.vb
@@ -0,0 +1,373 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.BtClose = New System.Windows.Forms.Button()
+        Me.BtNew = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Txtkembali = New System.Windows.Forms.TextBox()
+        Me.Txtjumlah = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Txtbiaya = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Txtlama = New System.Windows.Forms.TextBox()
+        Me.Txtharga = New System.Windows.Forms.TextBox()
+        Me.cbJenis = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Txtnama = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Txtdate = New System.Windows.Forms.DateTimePicker()
+        Me.Txtalamat = New System.Windows.Forms.TextBox()
+        Me.Txtnomer = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.BtClose)
+        Me.GroupBox3.Controls.Add(Me.BtNew)
+        Me.GroupBox3.Location = New System.Drawing.Point(50, 328)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(331, 194)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Navigasi"
+        '
+        'BtClose
+        '
+        Me.BtClose.Location = New System.Drawing.Point(173, 75)
+        Me.BtClose.Name = "BtClose"
+        Me.BtClose.Size = New System.Drawing.Size(138, 98)
+        Me.BtClose.TabIndex = 1
+        Me.BtClose.Text = "CLOSE"
+        Me.BtClose.UseVisualStyleBackColor = True
+        '
+        'BtNew
+        '
+        Me.BtNew.Location = New System.Drawing.Point(10, 75)
+        Me.BtNew.Name = "BtNew"
+        Me.BtNew.Size = New System.Drawing.Size(138, 98)
+        Me.BtNew.TabIndex = 0
+        Me.BtNew.Text = "NEW"
+        Me.BtNew.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Txtkembali)
+        Me.GroupBox4.Controls.Add(Me.Txtjumlah)
+        Me.GroupBox4.Controls.Add(Me.Label9)
+        Me.GroupBox4.Controls.Add(Me.Label8)
+        Me.GroupBox4.Location = New System.Drawing.Point(462, 310)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(372, 204)
+        Me.GroupBox4.TabIndex = 3
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Pembayaran"
+        '
+        'Txtkembali
+        '
+        Me.Txtkembali.Location = New System.Drawing.Point(178, 149)
+        Me.Txtkembali.Name = "Txtkembali"
+        Me.Txtkembali.Size = New System.Drawing.Size(156, 22)
+        Me.Txtkembali.TabIndex = 3
+        '
+        'Txtjumlah
+        '
+        Me.Txtjumlah.Location = New System.Drawing.Point(178, 49)
+        Me.Txtjumlah.Name = "Txtjumlah"
+        Me.Txtjumlah.Size = New System.Drawing.Size(156, 22)
+        Me.Txtjumlah.TabIndex = 2
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(21, 149)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(74, 17)
+        Me.Label9.TabIndex = 1
+        Me.Label9.Text = "Kembalian"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(24, 49)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(137, 17)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Jumlah Pembayaran"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Txtbiaya)
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Controls.Add(Me.Txtlama)
+        Me.GroupBox2.Controls.Add(Me.Txtharga)
+        Me.GroupBox2.Controls.Add(Me.cbJenis)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Location = New System.Drawing.Point(453, 72)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(381, 232)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Booking Price"
+        '
+        'Txtbiaya
+        '
+        Me.Txtbiaya.Location = New System.Drawing.Point(178, 172)
+        Me.Txtbiaya.Name = "Txtbiaya"
+        Me.Txtbiaya.Size = New System.Drawing.Size(156, 22)
+        Me.Txtbiaya.TabIndex = 7
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(21, 172)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(126, 17)
+        Me.Label12.TabIndex = 6
+        Me.Label12.Text = "Biaya Sewa Kamar"
+        '
+        'Txtlama
+        '
+        Me.Txtlama.Location = New System.Drawing.Point(178, 124)
+        Me.Txtlama.Name = "Txtlama"
+        Me.Txtlama.Size = New System.Drawing.Size(156, 22)
+        Me.Txtlama.TabIndex = 5
+        '
+        'Txtharga
+        '
+        Me.Txtharga.Location = New System.Drawing.Point(178, 87)
+        Me.Txtharga.Name = "Txtharga"
+        Me.Txtharga.Size = New System.Drawing.Size(156, 22)
+        Me.Txtharga.TabIndex = 4
+        '
+        'cbJenis
+        '
+        Me.cbJenis.FormattingEnabled = True
+        Me.cbJenis.Items.AddRange(New Object() {"VIP", "Kelas I", "Kelas II", "kelas III"})
+        Me.cbJenis.Location = New System.Drawing.Point(178, 49)
+        Me.cbJenis.Name = "cbJenis"
+        Me.cbJenis.Size = New System.Drawing.Size(156, 24)
+        Me.cbJenis.TabIndex = 3
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(21, 124)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(109, 17)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Lama Menginap"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(21, 87)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(134, 17)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Harga Sewa perhari"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(21, 49)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(86, 17)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Jenis Kamar"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nama Pemesan"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nomer Telpon"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(7, 121)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Alamat"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(-3, 215)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(114, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Tanggal booking"
+        '
+        'Txtnama
+        '
+        Me.Txtnama.Location = New System.Drawing.Point(140, 39)
+        Me.Txtnama.Name = "Txtnama"
+        Me.Txtnama.Size = New System.Drawing.Size(153, 22)
+        Me.Txtnama.TabIndex = 4
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Txtdate)
+        Me.GroupBox1.Controls.Add(Me.Txtalamat)
+        Me.GroupBox1.Controls.Add(Me.Txtnomer)
+        Me.GroupBox1.Controls.Add(Me.Txtnama)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(40, 72)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(341, 250)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "[Data Pemesan]"
+        '
+        'Txtdate
+        '
+        Me.Txtdate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.Txtdate.Location = New System.Drawing.Point(140, 210)
+        Me.Txtdate.Name = "Txtdate"
+        Me.Txtdate.Size = New System.Drawing.Size(171, 22)
+        Me.Txtdate.TabIndex = 8
+        '
+        'Txtalamat
+        '
+        Me.Txtalamat.Location = New System.Drawing.Point(140, 121)
+        Me.Txtalamat.Multiline = True
+        Me.Txtalamat.Name = "Txtalamat"
+        Me.Txtalamat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Txtalamat.Size = New System.Drawing.Size(171, 56)
+        Me.Txtalamat.TabIndex = 6
+        '
+        'Txtnomer
+        '
+        Me.Txtnomer.Location = New System.Drawing.Point(140, 82)
+        Me.Txtnomer.Name = "Txtnomer"
+        Me.Txtnomer.Size = New System.Drawing.Size(153, 22)
+        Me.Txtnomer.TabIndex = 5
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(0, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(59, 17)
+        Me.Label10.TabIndex = 5
+        Me.Label10.Text = "Label10"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(373, 25)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(103, 17)
+        Me.Label11.TabIndex = 6
+        Me.Label11.Text = "HOTEL ANJAY"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(886, 526)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Aplikasi pemesanan kamar hotel"
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Txtnama As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Txtdate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Txtalamat As System.Windows.Forms.TextBox
+    Friend WithEvents Txtnomer As System.Windows.Forms.TextBox
+    Friend WithEvents BtClose As System.Windows.Forms.Button
+    Friend WithEvents BtNew As System.Windows.Forms.Button
+    Friend WithEvents Txtjumlah As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Txtlama As System.Windows.Forms.TextBox
+    Friend WithEvents Txtharga As System.Windows.Forms.TextBox
+    Friend WithEvents cbJenis As System.Windows.Forms.ComboBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Txtkembali As System.Windows.Forms.TextBox
+    Friend WithEvents Txtbiaya As System.Windows.Forms.TextBox
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+
+End Class
